8a8e277c6e36e2d2f6e568aae8c44cb48e5541d7
Clément <clement@marcel.team>
2022-09-01 15:46:57 +0200

    Add dev mode.

--- src/components/Recipes/Recipes.vue
+++ src/components/Recipes/Recipes.vue
@@ -2,8 +2,38 @@
 </script>
 
 <template>
-    <div>Recettes : en développement ...</div>
+    <div>
+        <div class="Dialog_cls">
+            <h1>Onglet Recettes</h1>
+            <p>Cet onglet est en cours de développement.</p>
+        </div>
+    </div>
 </template>
 
 <style scoped>
+    .Dialog_cls
+    {
+        display:        inline-block;
+        position:       absolute;
+        padding:        20px;
+        border-radius:  5px;
+        border:         solid 2px #c8b273;
+        top:            50%;
+        left:           50%;
+        transform:      translate(-50%, -50%);
+    }
+
+    h1
+    {
+        color:        #c8b273;
+        font-family:    'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+        font-size:      1.5em;
+        text-align:     center;
+    }
+
+    p
+    {
+        font-family:    'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+        font-size:      1em;
+    }
 </style>
