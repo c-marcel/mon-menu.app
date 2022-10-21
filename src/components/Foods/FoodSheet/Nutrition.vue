ce8b0c63db5baf14c5b8c774e62d92c19664ad19
Clément <clement@marcel.team>
2022-10-21 15:30:04 +0200

    Add all Ciqual fields.

--- src/components/Foods/FoodSheet/Nutrition.vue
+++ src/components/Foods/FoodSheet/Nutrition.vue
@@ -10,28 +10,68 @@
     defineEmits(['changeNutritionData', 'changeNutritionDataSource'])
 
     const metadata = ref([
+        { "key": "energy_kj", "title": "Energie", "units": "kJ/100 g", "conversion": 10.0 },
         { "key": "energy", "title": "Energie", "units": "kcal/100 g", "conversion": 10.0 },
+        { "key": "water", "title": "Eau", "units": "g/100 g", "conversion": 0.01 },
+        { "key": "salt", "title": "Sel", "units": "g/100 g", "conversion": 0.01 },
+        { "key": "sodium", "title": "Sodium", "units": "mg/100 g", "conversion": 0.00001 },
+        { "key": "magnesium", "title": "Magnésium", "units": "mg/100 g", "conversion": 0.00001 },
+        { "key": "phosphorus", "title": "Phosphore", "units": "mg/100 g", "conversion": 0.00001 },
+        { "key": "potassium", "title": "Potassium", "units": "mg/100 g", "conversion": 0.00001 },
+        { "key": "calcium", "title": "Calcium", "units": "mg/100 g", "conversion": 0.00001 },
+        { "key": "manganese", "title": "Manganèse", "units": "mg/100 g", "conversion": 0.00001 },
+        { "key": "iron", "title": "Fer", "units": "mg/100 g", "conversion": 0.00001 },
+        { "key": "copper", "title": "Cuivre", "units": "mg/100 g", "conversion": 0.00001 },
+        { "key": "zinc", "title": "Zinc", "units": "mg/100 g", "conversion": 0.00001 },
+        { "key": "selenium", "title": "Sélénium", "units": "µg/100 g", "conversion": 0.00000001 },
+        { "key": "iodine", "title": "Iode", "units": "µg/100 g", "conversion": 0.00000001 },
         { "key": "proteins", "title": "Protéines", "units": "g/100 g", "conversion": 0.01 },
         { "key": "carbohydrates", "title": "Glucides", "units": "g/100 g", "conversion": 0.01 },
-        { "key": "lipids", "title": "Lipides", "units": "g/100 g", "conversion": 0.01 },
         { "key": "sugars", "title": "Sucres", "units": "g/100 g", "conversion": 0.01 },
+        { "key": "fructose", "title": "Fructose", "units": "g/100 g", "conversion": 0.01 },
+        { "key": "galactose", "title": "Galactose", "units": "g/100 g", "conversion": 0.01 },
+        { "key": "lactose", "title": "Lactose", "units": "g/100 g", "conversion": 0.01 },
+        { "key": "glucose", "title": "Glucose", "units": "g/100 g", "conversion": 0.01 },
+        { "key": "maltose", "title": "Maltose", "units": "g/100 g", "conversion": 0.01 },
+        { "key": "saccharose", "title": "Saccharose", "units": "g/100 g", "conversion": 0.01 },
+        { "key": "starch", "title": "Amidon", "units": "g/100 g", "conversion": 0.01 },
+        { "key": "polyols", "title": "Polyols", "units": "g/100 g", "conversion": 0.01 },
         { "key": "fibers", "title": "Fibres", "units": "g/100 g", "conversion": 0.01 },
-        { "key": "omega3_ala", "title": "Oméga 3 ALA", "units": "g/100 g", "conversion": 0.01 },
-        { "key": "omega3_epa", "title": "Oméga 3 EPA", "units": "g/100 g", "conversion": 0.01 },
-        { "key": "omega3_dha", "title": "Oméga 3 DHA", "units": "g/100 g", "conversion": 0.01 },
-        { "key": "omega6_la", "title": "Oméga 6 LA", "units": "g/100 g", "conversion": 0.01 },
-        { "key": "omega6_ara", "title": "Oméga 6 ARA", "units": "g/100 g", "conversion": 0.01 },
-        { "key": "omega9", "title": "Oméga 9", "units": "g/100 g", "conversion": 0.01 },
-        { "key": "salt", "title": "Sel", "units": "g/100 g", "conversion": 0.01 },
-        { "key": "calcium", "title": "Calcium", "units": "mg/100 g", "conversion": 0.00001 },
-        { "key": "copper", "title": "Cuivre", "units": "mg/100 g", "conversion": 0.00001 },
-        { "key": "iron", "title": "Fer", "units": "mg/100 g", "conversion": 0.00001 },
-        { "key": "iodine", "title": "Iode", "units": "µg/100 g", "conversion": 0.00000001 },
-        { "key": "magnesium", "title": "Magnésium", "units": "mg/100 g", "conversion": 0.00001 },
-        { "key": "sodium", "title": "Sodium", "units": "mg/100 g", "conversion": 0.00001 },
-        { "key": "zinc", "title": "Zinc", "units": "mg/100 g", "conversion": 0.00001 },
-        { "key": "vitamin_c", "title": "Vitamine C", "units": "mg/100 g", "conversion": 0.00001 },
+        { "key": "lipids", "title": "Lipides", "units": "g/100 g", "conversion": 0.01 },
+        { "key": "satured_fa", "title": "AG saturés", "units": "g/100 g", "conversion": 0.01 },
+        { "key": "monounsaturated_fa", "title": "AG monoinsaturés", "units": "g/100 g", "conversion": 0.01 },
+        { "key": "polyunsaturated_fa", "title": "AG polyinsaturés", "units": "g/100 g", "conversion": 0.01 },
+        { "key": "butyric_fa", "title": "AG butyrique", "units": "g/100 g", "conversion": 0.01 },
+        { "key": "caproic_fa", "title": "AG caproïque", "units": "g/100 g", "conversion": 0.01 },
+        { "key": "caprylic_fa", "title": "AG caprylique", "units": "g/100 g", "conversion": 0.01 },
+        { "key": "whimsical_fa", "title": "AG caprique", "units": "g/100 g", "conversion": 0.01 },
+        { "key": "lauric_fa", "title": "AG laurique", "units": "g/100 g", "conversion": 0.01 },
+        { "key": "myristic_fa", "title": "AG myristique", "units": "g/100 g", "conversion": 0.01 },
+        { "key": "palmitic_fa", "title": "AG palmitique", "units": "g/100 g", "conversion": 0.01 },
+        { "key": "stearic_fa", "title": "AG stéarique", "units": "g/100 g", "conversion": 0.01 },
+        { "key": "omega9", "title": "AG (n-9), oléique", "units": "g/100 g", "conversion": 0.01 },
+        { "key": "omega6_la", "title": "AG (n-6), linoléique", "units": "g/100 g", "conversion": 0.01 },
+        { "key": "omega3_ala", "title": "AG (n-3), alpha-lin.", "units": "g/100 g", "conversion": 0.01 },
+        { "key": "omega6_ara", "title": "AG (n-6), arachid.", "units": "g/100 g", "conversion": 0.01 },
+        { "key": "omega3_epa", "title": "AG (n-3) EPA", "units": "g/100 g", "conversion": 0.01 },
+        { "key": "omega3_dha", "title": "AG (n-3) DHA", "units": "g/100 g", "conversion": 0.01 },
+        { "key": "retinol", "title": "Rétinol", "units": "µg/100 g", "conversion": 0.00000001 },
+        { "key": "betacarotene", "title": "Béta-carotène", "units": "µg/100 g", "conversion": 0.00000001 },
         { "key": "vitamin_d", "title": "Vitamine D", "units": "µg/100 g", "conversion": 0.00000001 },
+        { "key": "vitamin_e", "title": "Vitamine E", "units": "mg/100 g", "conversion": 0.00001 },
+        { "key": "vitamin_k1", "title": "Vitamine K1", "units": "µg/100 g", "conversion": 0.00000001 },
+        { "key": "vitamin_k2", "title": "Vitamine K2", "units": "µg/100 g", "conversion": 0.00000001 },
+        { "key": "vitamin_c", "title": "Vitamine C", "units": "mg/100 g", "conversion": 0.00001 },
+        { "key": "vitamin_b1", "title": "Vitamine B1", "units": "mg/100 g", "conversion": 0.00001 },
+        { "key": "vitamin_b2", "title": "Vitamine B2", "units": "mg/100 g", "conversion": 0.00001 },
+        { "key": "vitamin_b3", "title": "Vitamine B3", "units": "mg/100 g", "conversion": 0.00001 },
+        { "key": "vitamin_b5", "title": "Vitamine B5", "units": "mg/100 g", "conversion": 0.00001 },
+        { "key": "vitamin_b6", "title": "Vitamine B6", "units": "mg/100 g", "conversion": 0.00001 },
+        { "key": "vitamin_b12", "title": "Vitamine B12", "units": "µg/100 g", "conversion": 0.00000001 },
+        { "key": "vitamin_b9", "title": "Vitamine B9", "units": "µg/100 g", "conversion": 0.00000001 },
+        { "key": "alcohol", "title": "Alcool", "units": "g/100 g", "conversion": 0.01 },
+        { "key": "organic_acids", "title": "Acides organiques", "units": "g/100 g", "conversion": 0.01 },
+        { "key": "cholesterol", "title": "Cholestérol", "units": "mg/100 g", "conversion": 0.00001 },
     ])
 </script>
 
@@ -87,8 +127,8 @@
     .NutritionGridEntry_cls
     {
         display:            inline-block;
-        width:              130px;
-        height:             60px;
+        width:              150px;
+        height:             80px;
         margin-left:        5px;
         margin-top:         5px;
         background-color: #834655;
@@ -110,6 +150,7 @@
     .NutritionGridEntryTitle_cls
     {
         color:          #c8b273;
+        font-size:      0.8em;
         font-weight:    bold;
         font-family:    'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
         text-align:     center;
